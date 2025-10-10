2e2fe3b80000466d6db891c1d10c86636046d91f
Srajavath <shireshar1@gmail.com>
2025-10-10 14:09:56 +0530

    Remove ActiveRecord settings for no-SQL app

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,6 @@
 require_relative "boot"
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-# Skip ActiveRecord since no SQL database is used
-# require "active_record/railtie"  # ❌ Not needed
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,9 +14,20 @@ module TodoApp
     # Only loads a smaller set of middleware suitable for API-only apps.
     config.api_only = true
 
+    # Disable Active Record since you are not using SQL
+    config.generators.orm :none
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # You can also disable Active Record comp
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
